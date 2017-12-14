8e6097dddad83d758eaa7409b0583e2722a46cda
MKonsta <kostyan.exe@gmail.com>
2017-12-14 17:29:09 +0600

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,19 @@
 package ru.job4j;
 
+/**
+*Package for calculate task
+*Класс для вычисления математичееских операций
+*
+* @author Konstantin Matushenko (xxx@mail.ru)
+* @version $Id$
+* @since 14.12.2017
+*/
+
 public class Calculate {
+	/**
+	* Method main is the start point of the program
+	* @parm args is the arrow. This is parametr
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
