4fe17ea544a713cd742ba4e5945ca7e60e122200
MKonsta <kostyan.exe@gmail.com>
2017-12-19 11:53:44 +0600

    2.3. Элементарный калькулятор. [#185]

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,22 +7,22 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Konstantin Matushenko (xxx@mail.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Konstantin Matushenko (xxx@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/ @Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Konstantin Matushenko";
-    String expect = "Echo, echo, echo : Konstantin Matushenko"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */ @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Konstantin Matushenko";
+        String expect = "Echo, echo, echo : Konstantin Matushenko";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest; {
+	@Test
+	public void whenAddOnePlusOneThenTwo() {
+		Calculator calc = new Calculator();
+		calc.add(1D, 1D);
+		double result = calc.getResult();
+		double expect = 2D;
+		assertThat(result, is(expected));
+	}
+	
+	public void whenSubtractOneMinesOneThenZero() {
+		Calculator calc = new Calculator();
+		calc.subtract(1D, 1D);
+		double result = calc.getResult();
+		double expect = 0D;
+		assertThat(result, is(expected));
+	}
+	
+	public void whenOneDivOneThenOne() {
+		Calculator calc = new Calculator();
+		calc.div(1D, 1D);
+		double result = calc.getResult();
+		double expect = 1D;
+		assertThat(result, is(expected));
+	}
+	
+	public void whenOneMultipleOneThenOne() {
+		Calculator calc = new Calculator();
+		calc.multiple(1D, 1D);
+		double result = calc.getResult();
+		double expect = 1D;
+		assertThat(result, is(expected));
+	}
+}
