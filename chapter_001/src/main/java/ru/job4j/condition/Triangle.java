d4fd201c25d019ef1f0d91d3c2c6f1339b1a891e
MKonsta <kostyan.exe@gmail.com>
2017-12-25 14:58:16 +0600

    3.3. Вычисление площади треугольника [#9461]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -42,4 +42,12 @@ public class Triangle {
 		return rsl;
 	}
 
+	/*public static void main(String[] args) {
+		Point a = new Point(0, 0);
+		Point b = new Point(0, 2);
+		Point c = new Point(2, 0);
+		Triangle triangle = new Triangle(a, b, c);
+		System.out.println(triangle.area());
+	}*/
+
 }
