7246d09e8fcb9c2cbb418f7ef37c7342579fd9c9
MKonsta <kostyan.exe@gmail.com>
2018-02-08 10:31:18 +0600

    Подкорректировал подклассы

--- chapter_002/src/main/java/ru/job4j/Pacient.java
+++ chapter_002/src/main/java/ru/job4j/Pacient.java
@@ -3,4 +3,7 @@ package ru.job4j;
 import ru.job4j.Profession;
 
 public class Pacient extends Profession {
+    public Pacient(String name, int age, String specialization) {
+        super(name, age, specialization);
+    }
 }
--- chapter_002/src/main/java/ru/job4j/Pupil.java
+++ chapter_002/src/main/java/ru/job4j/Pupil.java
@@ -3,4 +3,7 @@ package ru.job4j;
 import ru.job4j.Profession;
 
 public class Pupil extends Profession{
+    public Pupil(String name, int age, String specialization) {
+        super(name, age, specialization);
+    }
 }
