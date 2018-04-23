da4f926eb9f716fda93d4e8256c4a3e5d528319c
MKonsta <kostyan.exe@gmail.com>
2018-04-23 09:04:06 +0600

    кофемашина работает в 2 цикла

--- chapter_002/src/main/java/ru/job4j/CoffeMashine.java
+++ chapter_002/src/main/java/ru/job4j/CoffeMashine.java
@@ -2,50 +2,33 @@ package ru.job4j;
 
 public class CoffeMashine {
     int[] changes(int value, int price) {
+        int[] coins = {10, 5, 2, 1};
         int cha = value - price;
         int size = 0;
+        int i = 0;
         while (cha != 0) {
-            if (cha >= 10) {
-                cha -= 10;
+            if (cha >= coins[i]) {
+                cha -= coins[i];
                 size++;
-            } else if (cha >= 5) {
-                cha -= 5;
-                size++;
-            } else if (cha >= 2) {
-                cha -= 2;
-                size++;
-            } else if (cha >= 1) {
-                cha -= 1;
-                size++;
-            }
+            } else i++;
         }
-        int[] coins = new int[size];
+        int[] changeArray = new int[size];
         cha = value - price;
+        i = 0;
         int index = 0;
         while (cha != 0) {
-            if (cha >= 10) {
-                coins[index] = 10;
-                cha -= 10;
+            if (cha >= coins[i]) {
+                cha -= coins[i];
+                changeArray[index] = coins[i];
                 index++;
-            } else if (cha >= 5) {
-                coins[index] = 5;
-                cha -= 5;
-                index++;
-            } else if (cha >= 2) {
-                coins[index] = 2;
-                cha -= 2;
-                index++;
-            } else if (cha >= 1) {
-                coins[index] = 1;
-                cha -= 1;
-            }
+            } else i++;
         }
-        return coins;
+        return changeArray;
     }
 
     public static void main(String[] args) {
-        CoffeMashine coffeMashine = new CoffeMashine();
-        for (int i : coffeMashine.changes(50, 2)) {
+        CoffeMashine cof = new CoffeMashine();
+        for (int i : cof.changes(100, 48)) {
             System.out.println(i);
         }
     }
