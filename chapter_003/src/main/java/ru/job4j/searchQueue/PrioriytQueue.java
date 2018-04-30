1ad259cab6990ae9f3cff210551f1abdb80227dc
MKonsta <kostyan.exe@gmail.com>
2018-04-30 12:07:56 +0600

    2. Очередь с приоритетом на LinkedList [#41670]

---
+++ chapter_003/src/main/java/ru/job4j/searchQueue/PrioriytQueue.java
@@ -0,0 +1,42 @@
+package ru.job4j.searchQueue;
+
+import java.util.LinkedList;
+
+public class PrioriytQueue {
+
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        if (tasks.size() != 0) {
+            for (Task task1 : tasks) {
+                if (task.getPriority() <= task1.getPriority()) {
+                  tasks.add(tasks.indexOf(task1), task);
+                  break;
+                } else if (task1.equals(tasks.getLast())) {
+                    tasks.addLast(task);
+                    break;
+                }
+            }
+        } else {
+            tasks.addFirst(task);
+        }
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+
+    public static void main(String[] args) {
+        PrioriytQueue prioriytQueue = new PrioriytQueue();
+        prioriytQueue.put(new Task("buhgal", 5));
+        prioriytQueue.put(new Task("otk", 2));
+        prioriytQueue.put(new Task("kassa", 8));
+        prioriytQueue.put(new Task("podaji", 3));
+        prioriytQueue.put(new Task("administraciya", 10));
+        prioriytQueue.put(new Task("ohrana", 1));
+
+        for (Task task : prioriytQueue.tasks) {
+            System.out.println(task.getPriority() + " " + task.getDesk());
+        }
+    }
+}
