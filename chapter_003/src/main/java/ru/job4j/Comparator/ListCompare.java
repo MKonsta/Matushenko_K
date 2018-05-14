12896488e8d64dbeecdace39b8d77a40f3bd46f8
MKonsta <kostyan.exe@gmail.com>
2018-05-14 19:20:57 +0600

    3. Компаратор для строк. [#35008]

---
+++ chapter_003/src/main/java/ru/job4j/Comparator/ListCompare.java
@@ -0,0 +1,43 @@
+package ru.job4j.Comparator;
+
+import java.util.Comparator;
+
+/**
+ * 3. Компаратор для строк. [#35008]
+ * Очень часто возникает ситуация, когда нужно сравнить два слова.
+
+ У нас есть готовый метод String.compareTo.
+
+ В это задании нужно создать подобный метод самому.
+
+ Вам нужно реализовать компаратор для сравнения двух массивов символов.
+
+ Необходимо реализовать поэлементное сравнение двух списков, т.е. сравниваем элементы двух списков, находящихся
+ на одних и тех же позициях (по одним и тем же индексом). Сравнение в лексикографическом порядке.
+
+ В этом задании нельзя использовать метод String.compateTo.
+
+ Вы можете использовать
+
+ String.charAt(int index)
+
+ Integer.compare(int left, int right),
+
+ Character.compare(char left, char right);
+ */
+
+public class ListCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int result = 0;
+        for (int i = 0; i < left.length() && i < right.length(); i++) {
+            if (Character.compare(left.charAt(i), right.charAt(i)) != 0) {
+                return Character.compare(left.charAt(i), right.charAt(i));
+            }
+        }
+        if (Integer.compare(left.length(), right.length()) != 0) {
+            return Integer.compare(left.length(), right.length());
+        }
+        return result;
+    }
+}
