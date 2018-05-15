3162d27be271d642fe7d68b43820ba34e44756a3
MKonsta <kostyan.exe@gmail.com>
2018-05-15 12:01:22 +0600

    2. Сортировка User с использованием Comparator [#10036][#52846] Упростил ComparatorByAge

--- chapter_003/src/main/java/ru/job4j/sortirovka/sortirovka_comparator/ComparatorByAge.java
+++ chapter_003/src/main/java/ru/job4j/sortirovka/sortirovka_comparator/ComparatorByAge.java
@@ -5,12 +5,13 @@ import java.util.Comparator;
 public class ComparatorByAge implements Comparator<User> {
     @Override
     public int compare(User a, User b) {
-        if (a.getAge() > b.getAge()) {
+        /*if (a.getAge() > b.getAge()) {
             return 1;
         } else if (a.getAge() < b.getAge()) {
             return -1;
         } else {
             return 0;
-        }
+        }*/
+        return a.getAge() - b.getAge();
     }
 }
