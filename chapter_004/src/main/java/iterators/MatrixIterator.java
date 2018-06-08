2a0fd3327d8057b4ee017899d17237d980b2ed5c
MKonsta <kostyan.exe@gmail.com>
2018-06-08 22:54:40 +0600

    5.1.1. Итератор для двухмерного массива int[][] [#9539]

---
+++ chapter_004/src/main/java/iterators/MatrixIterator.java
@@ -0,0 +1,56 @@
+package iterators;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * 5.1.1. Итератор для двухмерного массива int[][] [#9539]
+ * Необходимо создать итератор для двухмерного массива.
+ *
+ * int[][] value = {
+ {1, 2}
+ {3, 4}
+ };
+
+ метод next = должен вернуть последовательно 1, 2, 3, 4.
+ */
+
+public class MatrixIterator implements Iterator {
+    private final int[][] values;
+
+    private int i = 0;
+    private int j = 0;
+
+    public MatrixIterator(int[][] valies) {
+        this.values = valies;
+    }
+
+
+    public boolean hasNext() {
+        boolean result = false;
+        if (i < values.length && j < values[i].length) {
+            result = true;
+        } else if (i < values.length - 1 && j == values[i].length) {
+            i++;
+            j = 0;
+            result = true;
+        } else if (i == values.length - 1 && j == values[i].length - 1) {
+            result = false;
+        }
+
+        return result;
+    }
+
+    public Integer next() {
+        if (hasNext()) {
+            j++;
+            return values[i][j - 1];
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}
