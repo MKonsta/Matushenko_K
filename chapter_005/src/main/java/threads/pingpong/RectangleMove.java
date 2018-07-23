e4b4575175bd3ef64e80337599d36304a0ea1cd6
MKonsta <kostyan.exe@gmail.com>
2018-07-23 22:37:34 +0600

    1. Пин-понг. [#1016]

---
+++ chapter_005/src/main/java/threads/pingpong/RectangleMove.java
@@ -0,0 +1,34 @@
+package threads.pingpong;
+
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+    private final Rectangle rect;
+
+    public RectangleMove(Rectangle rect) {
+        this.rect = rect;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            while (this.rect.getX() != 300) {
+                this.rect.setX(this.rect.getX() + 1);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            while (this.rect.getX() != 0) {
+                this.rect.setX(this.rect.getX() - 1);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
