8613801058242daac08249cceb5f6631607c7a00
MKonsta <kostyan.exe@gmail.com>
2018-07-30 15:57:36 +0600

    Сделал движение в один цикл

--- chapter_005/src/main/java/threads/pingpong/RectangleMove.java
+++ chapter_005/src/main/java/threads/pingpong/RectangleMove.java
@@ -11,23 +11,28 @@ public class RectangleMove implements Runnable {
 
     @Override
     public void run() {
+        int deltaX = 1;
+        int deltaY = 1;
         while (true) {
-            while (this.rect.getX() != 300) {
-                this.rect.setX(this.rect.getX() + 1);
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+
+            if (this.rect.getX() == 300) {
+                deltaX = -1;
+            } else if (this.rect.getX() == 0) {
+                deltaX = 1;
             }
 
-            while (this.rect.getX() != 0) {
-                this.rect.setX(this.rect.getX() - 1);
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            if (this.rect.getY() == 300) {
+                deltaY = -1;
+            } else if (this.rect.getY() == 0) {
+                deltaY = 1;
+            }
+
+            this.rect.setX(this.rect.getX() + deltaX);
+            this.rect.setY(this.rect.getY() + deltaY);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
